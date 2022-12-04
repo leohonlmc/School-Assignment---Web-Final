11355d1b4f1eb213acf0b6bca19ff5fcc5ecde7d
Leo <82315240+leohonlmc@users.noreply.github.com>
2022-12-04 02:17:38 -0500

    New project added

---
+++ welcome.php
@@ -0,0 +1,7 @@
+<?php
+echo '<h1> Submitted! </h1>';
+echo '<p> Thank you for reaching us! </p>';
+echo '<p> We will get back to you very soon! </p>';
+echo '<a href="index.html"> <button> Back</button></a>';
+echo '';
+?>
